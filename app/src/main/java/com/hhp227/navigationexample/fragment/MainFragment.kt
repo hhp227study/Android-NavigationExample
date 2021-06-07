d6af5faa08d46563cf0b0dd7e15a29021c972060
hhp227 <hong227@naver.com>
2021-06-07 23:55:26 +0900

    BottomNavigationView 적용

---
+++ app/src/main/java/com/hhp227/navigationexample/fragment/MainFragment.kt
@@ -0,0 +1,22 @@
+package com.hhp227.navigationexample.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import com.hhp227.navigationexample.R
+import kotlinx.android.synthetic.main.fragment_main.*
+
+class MainFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (requireActivity() as AppCompatActivity).setSupportActionBar(toolbar)
+    }
+}
