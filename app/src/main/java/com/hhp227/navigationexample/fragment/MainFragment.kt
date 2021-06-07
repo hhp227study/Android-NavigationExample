624cc9084072d117eb655ac789900a872928db8c
hhp227 <hong227@naver.com>
2021-06-08 02:04:41 +0900

    BottomNavigationView 적용

--- app/src/main/java/com/hhp227/navigationexample/fragment/MainFragment.kt
+++ app/src/main/java/com/hhp227/navigationexample/fragment/MainFragment.kt
@@ -1,11 +1,15 @@
 package com.hhp227.navigationexample.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.hhp227.navigationexample.R
 import kotlinx.android.synthetic.main.fragment_main.*
 
